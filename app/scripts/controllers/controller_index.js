91ee8f5100933ffa99b1a003da15bee405496506
Juan Camilo Herrera <juankmiloardila@gmail.com>
2017-08-10 21:18:12 -0500

    Se desarrollo funcionalidad de busqueda de peliculas

--- app/scripts/controllers/controller_index.js
+++ app/scripts/controllers/controller_index.js
@@ -1,14 +1,50 @@
-angular.module("MyApp",[])
+angular.module("MyApp",['infinite-scroll'])
 .controller("indexCtrl",function($scope,$http) {
-	$scope.nombre = "probando angular!";
 
-    //CARGAR VALORES DEL GENERO DEL LIBRO PARA PODER CARGARLOS EN EL SELECT GENERO
-    $http.get("../json/json_libro_genero.php")
-	.then(function(result) {
-        console.log(result);
-        $scope.datos_genero_libro = result.data;﻿
-    }, function(result) {
-        //some error
-        console.log(result);
-    });
+	// FUNCION QUE SE EJECUTA CUANDO SE DA CLICK A LA IMAGEN DE BUSCAR
+	$scope.buscarPelicula = function() {
+		// swal($scope.busqueda);
+		var busqueda = $scope.busqueda;
+		$scope.buscarPorNombre(busqueda);
+	}
+
+	// FUNCION QUE PERMITE LISTAR LAS PRIMERAS (9) PELICULAS DE LA BUSQUEDA
+	$scope.buscarPorNombre = function(nombre_peli) {
+		if ($scope.busqueda != undefined || nombre_peli=="a") {
+			$scope.lista_peliculas = [];
+			$scope.peliculas = [];
+			var url = "https://api.themoviedb.org/3/search/tv?api_key=b2907782d07859a652052d3bae537475&query="+nombre_peli;
+			$http.get(url).then(function(result) {
+		        console.log(result);
+		        $scope.cantidad_pelis = result.data.results.length;
+		        if ($scope.cantidad_pelis != 0) {
+			        $scope.lista_peliculas = result.data.results;﻿
+			        for(var i = 0; i < 9; i++) { //LISTAR LAS PRIMERAS 9
+			        	if ($scope.lista_peliculas[i] != undefined) {
+			        		$scope.peliculas.push($scope.lista_peliculas[i]);
+			        	}
+					}
+					console.log($scope.peliculas);
+		        }else{
+		        	swal(
+						'Error.',
+						'No existe información de su búsqueda!',
+						'error'
+			        );
+		        }
+		    }, function(result) {
+		        //some error
+		        console.log(result);
+		    });
+		}
+	}
+
+	// FUNCION QUE SE EJECUTA CUANDO SE HACE INFINITE SCROLL
+	$scope.cargarMasPeliculas = function() {
+		console.log($scope.cantidad_pelis);
+		// var last = $scope.peliculas[$scope.peliculas.length - 1];
+		// for(var i = 1; i <= 9; i++) {
+		// 	$scope.peliculas.push(last + i);
+		// }
+	};
 });
--- app/scripts/script_index.js
+++ app/scripts/script_index.js
@@ -1,20 +1,55 @@
 $(document).ready(function(){
+	click_img_search();
+	click_img_setup();
+	click_img_videos();
+	// mouse_hover_div_pelicula();
+});
+
+function click_img_search() {
 	$("#img_search").click(function(){
 		if( $('.container-input-search').is(":visible") ){ //esta visible
 			//si esta visible
-			$('.container-input-search').hide("slow"); //esconder
+			// $('.container-input-search').hide("slow"); //esconder
 		}else{
 			//si no esta visible
 			$('.container-input-search').show("slow"); //mostrar
 			$('#busqueda').focus();
 		}
   	});
+}
 
-  	$("#img_setup").click(function(){
+function click_img_setup() {
+	$("#img_setup").click(function(){
   		swal(
 			'Información',
 			'Opciones de configuración en desarrollo!',
 			'info'
         );
   	});
-});
+}
+
+function click_img_videos() {
+	$("#img_videos").click(function(){
+		swal(
+			'Información',
+			'Esta sección se encuentra en desarrollo!',
+			'info'
+        );
+  	});
+}
+
+function mouse_hover_div_pelicula() {
+	$(".prueba").hover(function() { //entra el mouse 
+		// alert("entra");
+	    $(".container-titulo").addClass("container-titulo-hover");
+	    $(".texto-titulo").addClass("texto-titulo-hover");
+	    $(".container-titulo").removeClass("container-titulo");
+	    $(".texto-titulo").removeClass("texto-titulo");
+	  }, function() { //se quita el mouse
+	    $(".container-titulo").addClass("container-titulo");
+	    $(".texto-titulo").addClass("texto-titulo");
+	    $(".container-titulo").removeClass("container-titulo-hover");
+	    $(".texto-titulo").removeClass("texto-titulo-hover");
+	  }
+	);
+}
